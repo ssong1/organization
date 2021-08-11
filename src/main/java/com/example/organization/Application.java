d227155cab22507a820e1fce7eac95b3b4a170f9
ssong1 <sjsjj83@naver.com>
2021-08-11 23:01:17 +0900

    불필요한 import 삭제

--- src/main/java/com/example/organization/Application.java
+++ src/main/java/com/example/organization/Application.java
@@ -1,6 +1,5 @@
 package com.example.organization;
 
-import com.example.organization.domain.department.Department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
