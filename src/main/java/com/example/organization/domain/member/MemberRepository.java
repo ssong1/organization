e078265a46dbbf1fd1eb41e14658c50ca13c1d3d
ssong1 <sjsjj83@naver.com>
2021-08-17 21:09:40 +0900

    멤버명으로 멤버 검색하는 쿼리 추가

--- src/main/java/com/example/organization/domain/member/MemberRepository.java
+++ src/main/java/com/example/organization/domain/member/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.example.organization.domain.member;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+    List<Member> findAllByNameContaining(String searchKeyword);
 
 }
