acf19cd0dc8c069ead105d56a7831e456c8d529e
ssong1 <sjsjj83@naver.com>
2021-08-17 21:09:28 +0900

    멤버명으로 부서 검색하는 쿼리 추가

--- src/main/java/com/example/organization/domain/department/DepartmentRepository.java
+++ src/main/java/com/example/organization/domain/department/DepartmentRepository.java
@@ -26,4 +26,18 @@ public interface DepartmentRepository extends JpaRepository<Department, Integer>
         + "FROM cte ORDER BY lvl DESC", nativeQuery = true)
     List<Department> findAllByNameContaining(@Param("searchKeyword") String searchKeyword);
 
+    @Query(value = "WITH RECURSIVE cte (id, code, name, parent_id, lvl) AS ("
+        + "  SELECT d.*, 1 AS lvl"
+        + "  FROM department d"
+        + "  WHERE EXISTS (SELECT 1 FROM member m WHERE d.id = m.department_id AND m.name LIKE %:searchKeyword%)"
+        + "  UNION ALL"
+        + "  SELECT p.*, c.lvl + 1"
+        + "  FROM department p"
+        + "  INNER JOIN cte c"
+        + "  ON p.id = c.parent_id"
+        + ")"
+        + "SELECT DISTINCT c.id, c.code, c.name, c.parent_id "
+        + "FROM cte c ORDER BY lvl DESC", nativeQuery = true)
+    List<Department> findAllByMemberNameContaining(@Param("searchKeyword") String searchKeyword);
+
 }
